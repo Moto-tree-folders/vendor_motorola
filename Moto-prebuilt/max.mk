48233ecff093ef74fa0a1f174c61d72fe5948a21
seba3567 <sebastian26481@gmail.com>
2021-05-03 11:56:21 -0400

    drop sofiar for now

--- Moto-prebuilt/max.mk
+++
@@ -1,8 +0,0 @@
-
-#waves
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.audio_fx.current=waves \
-persist.vendor.audio_fx.waves.maxxsense=true \
-persist.vendor.audio_fx.waves.processing=true \
-persist.vendor.audio_fx.waves.proc_twks=true \
-persist.vendor.audio_fx.waves.systrace=true \
